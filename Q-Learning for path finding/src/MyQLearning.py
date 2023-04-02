36b1aa23bc7163f68f1f6e22edd04f9cef485531
Tianchen Qu <T.QU@student.tudelft.nl>
2023-04-02 23:48:58 +0200

    update_Q

--- Q-Learning for path finding/src/MyQLearning.py
+++ Q-Learning for path finding/src/MyQLearning.py
@@ -1,6 +1,10 @@
 from QLearning import QLearning
-
+import numpy as np
 class MyQLearning(QLearning):
     def update_q(self, state, action, r, state_next, possible_actions, alfa, gamma):
         # TODO Auto-generated method stub
+        Q_old=self.get_q(state,action)
+        Q_next_best=np.amax(self.get_action_values(state_next,possible_actions))
+        Q_new=Q_old+alfa*(r+gamma(Q_next_best-Q_old))
+        self.set_q(state,action,Q_new)
         return
